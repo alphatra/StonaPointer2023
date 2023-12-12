2e29638dd4f658213015f50b3a5423b619ea287c
nkarolczyk <nataliakarolczyk5@gmail.com>
2023-12-12 20:14:27 +0100

    menu next

--- app/components/menu.js
+++ app/components/menu.js
@@ -1,33 +1,26 @@
-"use client";
-import Link from 'next/link';
-import styles from './style.css'
+import { useState } from 'react';
+
 export function Menu() {
+    const [activeItem, setActiveItem] = useState(null);
+    const menuItems = ['Wydarzenia', 'Blog', 'Poznaj nas', 'Projekty', 'Kontakt'];
+
+    let menuItemStyles = "mt-0 mr-2.5 pt-2.5 pb-2.5 pr-5 pl-5 text-center rounded-3xl o-underline text-black border border-gray-300 border-solid grow-0 shrink-0 cursor-pointer";
+    let menuItemStylesActive = `${menuItemStyles} grow bg-black text-slate-200 border border-gray-300 border-solid`;
+
     return (
-      <nav className="bg-gray-800 text-white p-4">
-        <ul className={styles.navbarcontainer}>
-        
-          <li>
-            <Link href="/" className="hover:bg-gray-700 px-3 py-2 rounded text-lime-500">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link href="/about" className="hover:bg-gray-700 px-3 py-2 rounded">
-              about
-            </Link>
-          </li>
-          <li>
-            <Link href="/contact" className="hover:bg-gray-700 px-3 py-2 rounded">
-              contact
-            </Link>
-          </li>
-          
-        </ul>
-      </nav>
+        <div className="text-xs flex items-center justify-start .bg-white py-2.5 px-5 my-7 mx-auto text-base">
+            {menuItems.map((item, index) => (
+                <a key={index}
+                   className={activeItem === index ? menuItemStylesActive : menuItemStyles}
+                   onClick={() => setActiveItem(index)}>
+                    {item}
+                </a>
+            ))}
+        </div>
     );
-  };
-  
-  export default Menu;
+}
+
+export default Menu;
 
 
   
