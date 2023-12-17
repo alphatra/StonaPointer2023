97a2bfd054643e8a87e22cb05c606abcf3d03ac8
ZMejer <zosiacastiel@gmail.com>
2023-12-17 14:14:27 +0100

    dodanie czcionek i zastosowanie tam gdzie trzeba

--- app/components/person.js
+++ app/components/person.js
@@ -1,8 +1,9 @@
 "use client";
-import Link from 'next/link';
+import '../styles/fonts.css';
 
 export function Person({name, surname, caption, imageSrc}) {
     const initials = `${name?.[0] || ''}${surname?.[0] || ''}`;
+
     return (
         <>
         <div className={`w-36 h-36 ml-5 rounded-xl bg-sky-600/70 border ${imageSrc == undefined ? 'border-sky-950' : 'border-neutral-950'} shadow-2xl flex items-center justify-center text-7xl`}>
@@ -13,11 +14,11 @@ export function Person({name, surname, caption, imageSrc}) {
             )}
         </div>
         <div className="bg-stone-950 text-white p-2 w-1/5 mt-8">
-            <p className="text-xl ml-5">{name} {surname}</p>
-            <p className="text-sm ml-5">//{caption}</p>
+            <p className="text-xl ml-5" style={{ fontFamily: 'Minecraft' }}>{name} {surname}</p>
+            <p className="text-sm ml-5" style={{ fontFamily: 'Minecraft' }}>// {caption}</p>
         </div>
         </>
     );
-  };
-  
-  export default Person;
+}
+
+export default Person;
--- app/page.js
+++ app/page.js
@@ -1,13 +1,18 @@
 'use client'
 import {Menu} from './components/menu'
 import {Plen} from './components/plen'
+import {Person} from './components/person'
+import {Member} from './components/member'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <>
     <Menu/>
-    <Plen/>
+    {// <Plen/>
+    }
+    <Person name="Imie" surname="Nazwisko" caption="podpis"/>
+    <Member name="Imie" surname="Nazwisko" caption="podpis" desc="Opis osoby"/>
     </>
   )
 }
