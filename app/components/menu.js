ac3ca2e8ac79d72c90aac0347e6061a095fcda4b
nkarolczyk <nataliakarolczyk5@gmail.com>
2023-12-19 09:54:11 +0100

    NEW MENU OMEGALUL

--- app/components/menu.js
+++ app/components/menu.js
@@ -1,14 +1,14 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 export function Menu() {
     const [activeItem, setActiveItem] = useState(null);
     const menuItems = ['Wydarzenia', 'Blog', 'Poznaj nas', 'Projekty', 'Kontakt'];
 
-    let menuItemStyles = "mt-0 mr-2.5 pt-2.5 pb-2.5 pr-5 pl-5 text-center rounded-3xl o-underline text-black border border-gray-300 border-solid grow-0 shrink-0 cursor-pointer";
-    let menuItemStylesActive = `${menuItemStyles} grow bg-black text-slate-200 border border-gray-300 border-solid`;
+    let menuItemStyles = "transition-all duration-300 mt-0 mr-2.5 pt-2.5 pb-2.5 pr-5 pl-5 text-center rounded-3xl text-black border border-gray-300 border-solid cursor-pointer";
+    let menuItemStylesActive = `${menuItemStyles} flex-grow text-slate-200 bg-black`;
 
     return (
-        <div className="text-xs flex items-center justify-start .bg-white py-2.5 px-5 my-7 mx-auto text-base">
+        <div className="flex text-xs items-center justify-start bg-white py-2.5 px-5 my-7 mx-auto text-base">
             {menuItems.map((item, index) => (
                 <a key={index}
                    className={activeItem === index ? menuItemStylesActive : menuItemStyles}
@@ -21,6 +21,5 @@ export function Menu() {
 }
 
 export default Menu;
-
 
   
